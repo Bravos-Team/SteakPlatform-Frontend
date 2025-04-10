5e52e98582b4d4d9760b20615cc30e8a675c5b17
Chấn Phong <doanchanphong0610@gmail.com>
2025-04-11 01:01:45 +0700

    #35 - Fixing, using cdn url for showing images, and delete loginform component, delete plugin svg-loader

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,10 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import svgLoader from 'vite-svg-loader'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss(), svgLoader()],
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
