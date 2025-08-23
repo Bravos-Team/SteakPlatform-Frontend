7042998529f620b280344376ffe64bfea06bc7f1
dcphong <doanchanphong0610@gmail.com>
2025-08-23 16:50:22 +0700

    logout bug - refetch game publisher state

--- src/components/store/nav/MobileTopBar.vue
+++ src/components/store/nav/MobileTopBar.vue
@@ -1,18 +1,25 @@
 <template>
   <header class="bg-[#101014] backdrop-blur-md mb-3 laptop:hidden">
     <div
-      class="flex justify-between items-center peer/header lg:mx-auto lg:w-full text-white px-4 gap-x-[20px] h-[72px]">
+      class="flex justify-between items-center peer/header lg:mx-auto lg:w-full text-white px-4 gap-x-[20px] h-[72px]"
+    >
       <!-- LOGO -->
       <div class="flex items-center justify-between peer/hover h-full">
         <div class="group/logo flex flex-row w-full items-center lg:me-5 bg-[#101014]">
           <div class="flex flex-row shrink-0 items-center lg:flex-none gap-x-[8px]">
             <router-link to="/" class="shrink-0">
-              <img src="https://ccdn.steak.io.vn/logo_steak.svg" class="w-8 object-contain h-auto lg:w-10" alt="" />
+              <img
+                src="https://ccdn.steak.io.vn/logo_steak.svg"
+                class="w-8 object-contain h-auto lg:w-10"
+                alt=""
+              />
             </router-link>
 
-            <img src="https://ccdn.steak.io.vn/down-line-white.svg"
+            <img
+              src="https://ccdn.steak.io.vn/down-line-white.svg"
               class="lg:group-hover/logo:rotate-180 w-[8px] mt-2 hidden lg:block object-cover lg:w-3 lg:ms-2 lg:mt-3 lg:relative transition-all"
-              alt="" />
+              alt=""
+            />
           </div>
         </div>
       </div>
@@ -28,54 +35,87 @@
           <Drawer :prevent-scroll-restoration="true" direction="top" :open="openDrawer">
             <drawer-trigger as-child>
               <button @click="openDrawer = true" class="block lg:hidden cursor-pointer">
-                <img src="https://ccdn.steak.io.vn/assets-menu-mobile-repo-white.svg" class="w-6.5" alt="" />
+                <img
+                  src="https://ccdn.steak.io.vn/assets-menu-mobile-repo-white.svg"
+                  class="w-6.5"
+                  alt=""
+                />
               </button>
             </drawer-trigger>
-            <drawer-content @interact-outside="openDrawer = false"
-              class="h-full outline-0 bg-white/10 backdrop-blur-xl py-10">
+            <drawer-content
+              @interact-outside="openDrawer = false"
+              class="h-full outline-0 bg-white/10 backdrop-blur-xl py-10"
+            >
               <div class="mx-auto w-full">
                 <drawer-title class="hidden">Menu</drawer-title>
-                <drawer-header class="text-3xl font-extrabold py-0 w-full flex justify-between flex-row">
+                <drawer-header
+                  class="text-3xl font-extrabold py-0 w-full flex justify-between flex-row"
+                >
                   <span>{{ $t('menu.base') }}</span>
                   <div>
-                    <router-link @click="openDrawer = false" v-if="!useUserProfilesStores().getAccessRight()"
+                    <router-link
+                      @click="openDrawer = false"
+                      v-if="!useUserProfilesStores().getAccessRight()"
                       :to="{ name: 'Login' }"
-                      class="text-lg bg-blue-400/70 transition-colors duration-300 hover:bg-blue-400/90 px-4 py-1 rounded-sm">
+                      class="text-lg bg-blue-400/70 transition-colors duration-300 hover:bg-blue-400/90 px-4 py-1 rounded-sm"
+                    >
                       {{ $t('auth.login') }}
                     </router-link>
-                    <div v-else
-                      class="flex items-center gap-x-2 text-lg cursor-pointer h-full font-black bg-white/10 px-6 py-1 rounded-sm">
+                    <div
+                      v-else
+                      class="flex items-center gap-x-2 text-lg cursor-pointer h-full font-black bg-white/10 px-6 py-1 rounded-sm"
+                    >
                       {{ useUserProfilesStores().getAccessRight() }}
                     </div>
                   </div>
                 </drawer-header>
                 <drawer-description class="hidden"> </drawer-description>
                 <drawer-footer class="flex flex-col gap-y-2">
-                  <router-link v-if="useUserProfilesStores().getAccessRight()" :to="{ name: 'UserProfiles' }"
+                  <router-link
+                    v-if="useUserProfilesStores().getAccessRight()"
+                    :to="{ name: 'UserProfiles' }"
                     @click="openDrawer = false"
-                    class="flex justify-between gap-x-2 items-center w-full text-lg font-mono bg-white/5 px-3 py-1 rounded-xs hover:bg-white/10 focus:bg-white/20">
+                    class="flex justify-between gap-x-2 items-center w-full text-lg font-mono bg-white/5 px-3 py-1 rounded-xs hover:bg-white/10 focus:bg-white/20"
+                  >
                     <span class="text-center align-middle">
-                      {{ $t('auth.informations.user.profile.title') }}</span>
+                      {{ $t('auth.informations.user.profile.title') }}</span
+                    >
                     <UserStar class="text-white" />
                   </router-link>
-                  <router-link @click="openDrawer = false"
+                  <router-link
+                    @click="openDrawer = false"
                     class="w-full text-lg font-mono bg-white/5 px-3 py-1 rounded-xs hover:bg-white/10 focus:bg-white/20"
-                    :to="{ name: 'store-home' }">{{ $t('navigation.store') }}</router-link>
-                  <router-link @click="openDrawer = false"
+                    :to="{ name: 'store-home' }"
+                    >{{ $t('navigation.store') }}</router-link
+                  >
+                  <router-link
+                    @click="openDrawer = false"
                     class="w-full text-lg font-mono bg-white/5 px-3 py-1 rounded-xs hover:bg-white/10 focus:bg-white/20"
-                    :to="{ name: 'PublisherDashboard' }">{{ $t('navigation.development_workspace') }}</router-link>
-                  <router-link @click="openDrawer = false"
+                    :to="{ name: 'PublisherDashboard' }"
+                    >{{ $t('navigation.development_workspace') }}</router-link
+                  >
+                  <router-link
+                    @click="openDrawer = false"
                     class="w-full text-lg font-mono bg-white/5 px-3 py-1 rounded-xs hover:bg-white/10 focus:bg-white/20"
-                    :to="{ name: 'SupportCenter' }">{{ $t('navigation.help') }}</router-link>
-                  <button :class="{
-                    '!cursor-not-allowed opacity-50': !invalidDevice,
-                    'cursor-pointer': invalidDevice,
-                  }" :disabled="!invalidDevice" @click="handleRedirectDownload"
-                    class="w-full text-lg text-start font-mono bg-blue-300/20 px-3 py-1 rounded-xs hover:bg-blue-400/30 transition-all duration-300 font-black cursor-pointer">
+                    :to="{ name: 'SupportCenter' }"
+                    >{{ $t('navigation.help') }}</router-link
+                  >
+                  <button
+                    :class="{
+                      '!cursor-not-allowed opacity-50': !invalidDevice,
+                      'cursor-pointer': invalidDevice,
+                    }"
+                    :disabled="!invalidDevice"
+                    @click="handleRedirectDownload"
+                    class="w-full text-lg text-start font-mono bg-blue-300/20 px-3 py-1 rounded-xs hover:bg-blue-400/30 transition-all duration-300 font-black cursor-pointer"
+                  >
                     {{ $t('Download') }}
                   </button>
-                  <button v-if="useUserProfilesStores().getAccessRight()" @click="handleLogout"
-                    class="w-full flex flex-row-reverse justify-between cursor-pointer gap-x-2 text-lg font-mono bg-white/5 px-3 py-1 rounded-xs hover:bg-white/10 focus:bg-white/20">
+                  <button
+                    v-if="useUserProfilesStores().getAccessRight()"
+                    @click="handleLogout"
+                    class="w-full flex flex-row-reverse justify-between cursor-pointer gap-x-2 text-lg font-mono bg-white/5 px-3 py-1 rounded-xs hover:bg-white/10 focus:bg-white/20"
+                  >
                     <LoaderCircle v-if="isLogoutPending" class="animate-spin" />
                     <LogOut v-else class="text-white" />
                     {{ $t('auth.logout') }}
@@ -105,8 +145,11 @@ import LanguagesOption from '@/components/common/LanguagesOption.vue'
 import { getCookie, removeCookie } from '@/utils/cookies/cookie-utils'
 import { LoaderCircle, LogOut, UserStar } from 'lucide-vue-next'
 import { useRouter } from 'vue-router'
-import { toastErrorNotificationPopup, toastSuccessNotificationPopup } from '@/composables/toast/toastNotificationPopup'
-import { computed, ref } from 'vue'
+import {
+  toastErrorNotificationPopup,
+  toastSuccessNotificationPopup,
+} from '@/composables/toast/toastNotificationPopup'
+import { computed, nextTick, ref } from 'vue'
 import { useMutateUserLogout } from '@/hooks/store/auth/useAuthentications'
 import { useUserProfilesStores } from '@/stores/user/useUserProfiles'
 const { isPending: isLogoutPending, mutateAsync: mutateLogout } = useMutateUserLogout()
@@ -114,22 +157,25 @@ const { isPending: isLogoutPending, mutateAsync: mutateLogout } = useMutateUserL
 const router = useRouter()
 const openDrawer = ref(false)
 const handleLogout = async () => {
-  removeCookie('userAccessRights')
+  useUserProfilesStores().removeAccessRight()
   try {
     const response = await mutateLogout()
     if (response.status === 200) {
-      await router.push({ name: 'Login' })
-      toastSuccessNotificationPopup('Logout successfully', '')
-    }
-    else
-      toastErrorNotificationPopup('Logout failed', 'An error occurred while logging out.')
+      await nextTick(async () => {
+        await router.push({ name: 'Login' })
+        toastSuccessNotificationPopup('Logout successfully', '')
+      })
+    } else toastErrorNotificationPopup('Logout failed', 'An error occurred while logging out.')
   } catch (err: any) {
     toastErrorNotificationPopup('Logout failed', err.response.data.detail)
     console.log(err)
   }
 }
 const invalidDevice = computed(
-  () => navigator.platform.includes('Windows') || navigator.platform.includes('Win32') || navigator.platform.includes('Linux'),
+  () =>
+    navigator.platform.includes('Windows') ||
+    navigator.platform.includes('Win32') ||
+    navigator.platform.includes('Linux'),
 )
 const handleRedirectDownload = async () => {
   await router.push({ name: 'DownloadApplicationPage' })
